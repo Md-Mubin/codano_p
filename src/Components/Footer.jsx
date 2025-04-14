edd20969b57088e6d3ed03f24f515e4b55152413
Md Mubin <fontdevmubin@gmail.com>
2025-04-15 01:18:21 +0600

    Changes to be committed: 	modified:   src/Components/Footer.jsx

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -27,20 +27,21 @@ const Footer = () => {
         setSubmiting(true)
 
         // posting data to backend server to store in MongoDB database
-        try {
-            await axios.post("https://testserver1-stww.onrender.com/api/v1/emailSubscribers", { email })
-                .then(function (response) {
-                    if (response.status === 200 && response.data) {
-                        setMsg(response.data.success)
-                    }
+        await axios.post("https://testserver1-stww.onrender.com/api/v1/emailSubscribers", { email })
+            .then(function (response) {
+                if (response.status === 200 && response.data) {
+                    setMsg(response.data.success)
+                }
 
-                    else if (response.status === 400 && response.data) {
-                        setMsg(response.data.errMsg)
-                    }
-                })
-        } catch (error) {
-            setMsg(response.data.serverErr)
-        }
+                else if (response.status === 400 && response.data) {
+                    setMsg(response.data.errMsg)
+                }
+
+                else if(response.status === 400 && response.data){
+                    setMsg(response.data.serverErr)
+                }
+            })
+
 
         setTimeout(() => {
             setMsg("")
