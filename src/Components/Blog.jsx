661140c7c5049d7285e7484462ddf756d06d03ea
Md Mubin <fontdevmubin@gmail.com>
2025-04-14 12:15:27 +0600

    Changes to be committed: 	modified:   public/locales/de.json 	modified:   public/locales/en.json 	modified:   src/Components/Blog.jsx

--- src/Components/Blog.jsx
+++ src/Components/Blog.jsx
@@ -1,25 +1,16 @@
 "use client"
 import CommonHead from '@/Commons/CommonHead'
-import React from 'react'
+import React, { useState } from 'react'
 import { useLanguage } from '../../public/contexts/LanguageContext'
+import { MdOutlineCancel } from "react-icons/md"
 
 const Blog = () => {
 
     const { t } = useLanguage()
 
     const blogCards = t.blog?.blogCards || []
-
-    const blogImg = [
-        {
-            image: "https://images.unsplash.com/photo-1657812159055-7bae416f386d?q=80&w=2144&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-        },
-        {
-            image: "https://images.unsplash.com/photo-1526628953301-3e589a6a8b74?q=80&w=2006&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-        },
-        {
-            image: "https://images.unsplash.com/photo-1709120395858-92f1c7c577f5?q=80&w=1932&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-        }
-    ]
+    const [showCard, setShowCard] = useState(null)
+    console.log(blogCards)
 
     return (
         <>
@@ -31,23 +22,19 @@ const Blog = () => {
 
                     <div className='flex flex-col xl:flex-row justify-between gap-10 mt-24'>
                         {
-                            blogCards.map((items, index) => (
+                            blogCards.map((items) => (
                                 <ul
                                     key={items.id}
                                     className='shadow-lg hover:shadow-xl pb-10 rounded-2xl overflow-hidden'>
 
                                     {/* blog images */}
                                     <li>
-                                        {
-                                            blogImg[index] && (
-                                                <img
-                                                    className='w-full h-[300px]'
-                                                    loading='lazy'
-                                                    src={blogImg[index].image}
-                                                    alt={`Project ${index + 1}`}
-                                                />
-                                            )
-                                        }
+                                        <img
+                                            className='w-full h-[300px]'
+                                            loading='lazy'
+                                            src={items.img}
+                                            alt="blog images"
+                                        />
                                     </li>
 
                                     <li className='p-6'>
@@ -63,10 +50,39 @@ const Blog = () => {
                                     </li>
 
                                     <li className='mt-4 ml-6'>
-                                        <button className='px-6 py-1 rounded-full bg-blue-100 hover:bg-blue-200 duration-200 cursor-pointer'>Read More</button>
+                                        <button onClick={() => setShowCard(items)} className='px-6 py-1 rounded-full bg-blue-100 hover:bg-blue-200 duration-200 cursor-pointer'>Read More</button>
                                     </li>
                                 </ul>
                             ))
+                        }
+
+                        {/* read more card */}
+                        {
+                            showCard && (
+                                <div onClick={() => setShowCard(null)} className="fixed inset-0 bg-[#000000cc] flex justify-center items-center z-50">
+                                    <ul onClick={(e) => e.stopPropagation()} className='w-[1200px] p-10 bg-[#fff] rounded-xl relative z-[100]'>
+                                        <li>
+                                            <img className='w-[500px] h-[300px]' src={showCard.img} alt="" />
+                                        </li>
+
+                                        <li className='p-6'>
+                                            {showCard.date}
+                                        </li>
+
+                                        <li className='mt-2 pl-6 font-bold text-lg tracking-widest'>
+                                            {showCard.blogHead}
+                                        </li>
+
+                                        <li className='mt-4 px-6 font-medium text-[16px] tracking-widest'>
+                                            {showCard.fullDescription}
+                                        </li>
+
+                                        <li onClick={()=>setShowCard(null)} className='absolute top-10 right-10 text-4xl hover:text-[#598bc7] cursor-pointer'>
+                                            <MdOutlineCancel />
+                                        </li>
+                                    </ul>
+                                </div>
+                            )
                         }
                     </div>
                 </div>
