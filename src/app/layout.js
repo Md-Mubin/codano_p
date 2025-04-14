8aea6fee99f22e1624490e444f9c7423a6b08316
Md Mubin <fontdevmubin@gmail.com>
2025-04-14 23:46:53 +0600

    Changes to be committed: 	modified:   src/app/layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,11 +1,8 @@
 import "./globals.css";
 import Navbar from "@/Components/Navbar";
 import { Roboto } from 'next/font/google';
-// import Footer from "@/Components/Footer";
+import Footer from "@/Components/Footer";
 import { LanguageProvider } from "../../public/contexts/LanguageContext";
-import dynamic from "next/dynamic";
-
-const Footer = dynamic(() => import("@/Components/Footer"), { ssr: false });
 
 const interFont = Roboto({ weight: "400", preload: false })
 
