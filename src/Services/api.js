0ab03125ceb102e032cb04dd9a87317421edc043
Md Mubin <fontdevmubin@gmail.com>
2025-05-01 22:15:46 +0600

    Changes to be committed: 	modified:   src/Services/api.js

--- src/Services/api.js
+++ src/Services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL : import.meta.env.VITE_BACK_BASE_API,
+    baseURL : process.env.NEXT_BACK_BASE_API,
     headers : {
         "Content-Type" : "application/json"
     }
