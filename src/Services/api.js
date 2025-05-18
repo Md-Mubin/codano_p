4c868cb5b89613d787a21186dbacb217dbad61a9
Md Mubin <fontdevmubin@gmail.com>
2025-05-18 21:50:49 +0600

    modified:   src/Services/api.js

--- src/Services/api.js
+++ src/Services/api.js
@@ -1,18 +1,25 @@
-import axios from "axios";
+import axios from "axios"
 
 // api
 const api = axios.create({
-    baseURL : process.env.NEXT_PUBLIC_BACK_BASE_API,
-    headers : {
-        "Content-Type" : "application/json"
+    baseURL: process.env.NEXT_PUBLIC_BACK_END_BASE_API,
+    headers: {
+        "Content-Type": "application/json"
     }
 })
 
-// variables
+// subscription
 export const subscription = {
-    emailSub : async (email)=>{
-        const res = await api.post("/emailSubscribers", {email})
-        console.log(res)
+    emailSub: async (email) => {
+        const res = await api.post("/emailSubscribers", { email })
+        return res.data
+    }
+}
+
+// inquries
+export const inquires = {
+    inquiresData: async (name, email, phone, companyName, budget, description) => {
+        const res = await api.post("/inquiresDatas", { name, email, phone, companyName, budget, description })
         return res.data
     }
 }
